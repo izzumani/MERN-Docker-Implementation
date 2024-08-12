195db9615c84554360bfb52af2cfa5efda72c1b2
isaac njiru <isaac.njiru@gmail.com>
2024-08-11 23:11:04 -0600

    Implementation of docker for the mern

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Blog } from './Blog.jsx'
+const queryClient = new QueryClient()
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Blog />
+    </QueryClientProvider>
+  )
+}
